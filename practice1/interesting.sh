57218d10e87e367290a76b59e86efe0eb0f69001
Na Zhou <na.zhou.xyz@gmail.com>
2021-10-22 17:47:24 -0400

    test

---
+++ practice1/interesting.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# variable ${word} is passed as a parameter from the Jenkins job
+echo "Fascinating is a word I use for the unexpected, in this case I would think ${word} would suffice"
